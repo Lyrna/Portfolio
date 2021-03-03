a75a003caf6f1aff34387c84c502b9c8a1662ba2
Lyrna <clarisse.hurion@gmail.com>
2021-03-03 18:31:26 +0100

    illustration avancée et couleurs réarrangées

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,15 @@
-const button_javascript = document.querySelector("p#js");
-const js = document.querySelectorAll('div.box.js');
+$(document).ready(function(){
+	$('.tri').click(function(){
+		const value = $(this).attr('data-filter');
+			if (value == 'all'){
+				$('.box').show('1000');
+			} else {
+				$('.box').not('.'+value).hide('1000');
+				$('.box').filter('.'+value).show('1000');
+			}
+	})
 
-button_javascript.addEventListener('click', () => {
-	js.classList.toggle('is-visible');
-});
+	$('.tri').click(function(){
+		$(this).addClass('active').siblings().removeClass('active');
+	})
+})
